1805b3fc5dcdb33960d5500294295a4fec674b7f
silent_c0ntr0l <silent_control@tut.by>
2015-12-12 12:09:52 +0200

    Remove OPN, Added Description

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,118 @@
-var gulp = require("gulp"),
-		opn = require("opn"),
-		sass = require('gulp-sass'),
-		autoprefixer = require('gulp-autoprefixer'),
-		bourbon = require('node-bourbon'),
-		uglify = require('gulp-uglify'),
-		minifyCss = require('gulp-minify-css'),
-		rename = require("gulp-rename"),
-		wiredep = require('wiredep').stream,
-		gulpif = require('gulp-if'),
-		clean = require('gulp-clean'),
-		useref = require('gulp-useref'),
-		browserSync = require('browser-sync').create();
+/*******************************************************************************\
+		1.	DEPENDENCIES
+\*******************************************************************************/
 
-// Server
-gulp.task('connect', ['watch'], function() {
+var gulp = require("gulp"),																// gulp core
+		sass = require('gulp-sass'),													// sass compiler
+		gulpif = require('gulp-if'),													// conditionally run a task
+		clean = require('gulp-clean'),												// removing files and folders
+		uglify = require('gulp-uglify'),											// uglifies the js
+		rename = require("gulp-rename"),											// rename files
+		useref = require('gulp-useref'),											// parse build blocks in HTML files to replace references
+		bourbon = require('node-bourbon'),										// bourbon libruary
+		wiredep = require('wiredep').stream,									// bower dependencies to your source code
+		minifyCss = require('gulp-minify-css'),								// minify the css files
+		autoprefixer = require('gulp-autoprefixer'),					// sets missing browserprefixes
+		browserSync = require('browser-sync').create();				// inject code to all devices
+
+/*******************************************************************************\
+		2.	BROWSERSYNC (LOCAL SERVEVR)
+\*******************************************************************************/
+
+gulp.task('connect', ['watch'], function() {							// files to inject
 	 browserSync.init({
 		server: {
-			baseDir: "./app/"
+			baseDir: "./app/"																		// base dir
 		}
 	});
 });
 
-// Watch
+/*******************************************************************************\
+		3.	WATCHER (WATCHING FILE CHANGES)
+\*******************************************************************************/
+
 gulp.task('watch', function () {
-	gulp.watch(['./app/*.html'], ['html']),
-	gulp.watch(['./app/sass/*.scss'], ['scss']),
-	gulp.watch(['./app/js/*.js'], ['js']);
+	gulp.watch(['./app/*.html'], ['html']),									// watching changes in HTML
+	gulp.watch(['./app/sass/*.scss'], ['scss']),						// watching changes in SASS
+	gulp.watch(['./app/js/*.js'], ['js']);									// watching changes in JS
 });
 
-// HTML
+/*******************************************************************************\
+		4.	HTML TASKS
+\*******************************************************************************/
+
 gulp.task('html', function () {
-	gulp.src('./app/index.html')
+	gulp.src('./app/index.html')														// get the files
 		.pipe(wiredep({
-			directory: "./app/bower/"
+			directory: "./app/bower/"														// dir where wiredep get files 
 		}))
-		.pipe(gulp.dest('./app/'))
-		.pipe(browserSync.stream());
+		.pipe(gulp.dest('./app/'))														// where to put the file
+		.pipe(browserSync.stream());													// browsersync stream
 });
 
-// CSS
+/*******************************************************************************\
+		5.	SASS TASKS
+\*******************************************************************************/
+
 gulp.task('scss', function () {
-	gulp.src('./app/sass/*.scss')
+	gulp.src('./app/sass/*.scss')														// get the files
 		.pipe(sass({includePaths: require('node-bourbon').includePaths}).on('error', sass.logError))
-		.pipe(autoprefixer({browsers: ['last 5 versions'], cascade: false}))
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.stream());
+		.pipe(autoprefixer({browsers: ['last 3 versions'], cascade: false}))
+		.pipe(gulp.dest('app/css'))														// where to put the file
+		.pipe(browserSync.stream());													// browsersync stream
 });
 
-// JS
+/*******************************************************************************\
+		6.	JS TASKS
+\*******************************************************************************/
+
 gulp.task('js', function() {
-	return gulp.src('./app/js/common.js')
-		.pipe(browserSync.stream()); 
+	return gulp.src('./app/js/common.js')										// get the files
+		.pipe(browserSync.stream()); 													// browsersync stream
 });
 
-// Images
+/*******************************************************************************\
+		7.	IMAGES TASKS
+\*******************************************************************************/
+
 gulp.task('images', function () {
-		return gulp.src('./app/img/**/*')
-				.pipe(gulp.dest('dist/img'))
+		return gulp.src('./app/img/**/*')											// get the files
+				.pipe(gulp.dest('dist/img'))											// where to put the file
 });
 
-// Fonts
+/*******************************************************************************\
+		8.	FONTS TASKS
+\*******************************************************************************/
+
 gulp.task('fonts', function () {
-		return gulp.src('./app/fonts/**/*')
-				.pipe(gulp.dest('dist/fonts'))
+		return gulp.src('./app/fonts/**/*')										// get the files
+				.pipe(gulp.dest('dist/fonts'))										// where to put the file
 });
 
-// Libs
+/*******************************************************************************\
+		9.	LIBS TASKS (PERSONAL DEVELOPER LIBS)
+\*******************************************************************************/
+
 gulp.task('libs', function () {
-		return gulp.src('./app/libs/**/*')
-				.pipe(gulp.dest('dist/libs'))
+		return gulp.src('./app/libs/**/*')										// get the files
+				.pipe(gulp.dest('dist/libs'))											// where to put the file
 });
+
+/*******************************************************************************\
+		10.	BUILD TASKS
+\*******************************************************************************/
 
 // Clean
 gulp.task('clean', function () {
 		return gulp.src('dist', {read: false})
-				.pipe(clean());
+				.pipe(clean());																		// clean dir
 });
 
 // Build
 gulp.task('build', ['clean'], function () {
-		gulp.start('images');
-		gulp.start('fonts');
-		gulp.start('libs');
+		gulp.start('images');																	// images task
+		gulp.start('fonts');																	// fonts task
+		gulp.start('libs');																		// libs task
 		var assets = useref.assets();
 		 return gulp.src('app/*.html')
 				.pipe(assets)
@@ -92,5 +123,8 @@ gulp.task('build', ['clean'], function () {
 				.pipe(gulp.dest('./dist'));
 });
 
-// Local
+/*******************************************************************************\
+		11.	DEFAULT TASKS
+\*******************************************************************************/
+
 gulp.task('default', ['connect', 'watch']);
